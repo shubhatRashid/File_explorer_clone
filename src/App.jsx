b3cd53701548795f7d9974fdb4e99f6e5a85c983
shubhat33 <rashidshubhat41@gmail.com>
2024-03-23 11:44:08 +0530

    final commit project complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Recursion from './components/Recursion'
 function App() {
   const [data,setData] = useState(folderData)
   return (
-    <div style={{display:'flex',flexDirection:'column'}}>
+    <div style={{display:'flex',flexDirection:'column',justifyContent:"center",alignItems:'center'}}>
           <h1>Explorer</h1>
           <Recursion item={data[0]} parent={data[0]} index={0} margin = "10px" allData = {data} setAllData={setData}/>
     </div>
